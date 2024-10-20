a631eb457d69974e76285367157ce2effafa4d56
saathvik-sheerla <saathvik.sheerla@gmail.com>
2024-10-20 20:30:32 +0530

    developed basic getting of recOfMovies, working perfectly fine but structure and style has to be improved it's very basic but functionality is great.

--- app.js
+++ app.js
@@ -1,85 +1,159 @@
-const express = require('express');
-const ejs = require('ejs');
-const wiki = require('wikipedia');
-const {capitalize} = require('./capitalize');
+const express = require("express");
+const ejs = require("ejs");
+const wiki = require("wikipedia");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI("AIzaSyDgrPgEeLdK8NlAOc5cj4Uc3DCMohzEjk0");
+const { capitalize } = require("./capitalize");
+let { moviesData } = require("./movies");
 const app = express();
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
+  bodyParser.urlencoded({
     extended: true,
-    }),
+  }),
 );
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'views'));
-app.use(express.static(path.join(__dirname,'/public')));
-app.set(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.set(express.urlencoded({ extended: true }));
 
 const port = 5500;
 
-let movies = [
-    {
-        title: 'Jersey',
-        img: 'https://tse4.mm.bing.net/th?id=OIP.cQRTJKDBwz_4Avm9WNr2bwHaDt&pid=Api&P=0&h=180',
-    },
-    {
-        title: 'Seetharamam',
-        img: 'https://tse3.mm.bing.net/th?id=OIP.eYISEheQjk7FSFBkjpVZUAHaEK&pid=Api&P=0&h=180',
-    },
-    {
-        title: '12th Fail',
-        img: 'https://tse3.mm.bing.net/th?id=OIP.H6r_tEEEC34rwPUL8l_U_AAAAA&pid=Api&P=0&h=180',
-    },
-    {
-        title: 'Gifted',
-        img: 'https://3.bp.blogspot.com/-sDWFOoAbL54/WU2JT0crVLI/AAAAAAAAEGU/ALJzeckQvEcVrqd9WiS3gTg3ZEKMTm3OwCLcBGAs/s1600/QUAD_UK_Gifted_f.jpg',
-    },
-    {
-        title: 'Dangal',
-        img: 'https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2018/01/featured-dangal-1514956990.jpg',
-    },
-];
-
-async function getMovieInfo(name){
-	try {
-		const page = await wiki.page(name);
-		// console.log(page);
-		//Response of type @Page object
-        const summary = await page.summary();
-        // console.log(summary);
-        return summary;
-		// console.log(response);
-		//Response of type @wikiSummary - contains the intro and the main image
-	} catch (error) {
-		console.log(error);
-		//=> Typeof wikiError
-	}
-};
-
-app.get('/', (req, res)=>{
-    res.send('same-dramas....');
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  generationConfig: {
+    responseMimeType: "application/json",
+  },
 });
 
-app.post('/movie', async (req, res)=>{
-    let movieName = req.body.moviename;
-    movieName = capitalize(movieName);
-    const response = await getMovieInfo(movieName);
-    if(response && response.originalimage && response.title){
-        let newMovie = {
-            title: response.title,
-            img: response.originalimage.source,
+async function getSimilarMovies(movie) {
+  const result =
+    await model.generateContent(
+      `suggest exactly 3 similar movies to this movie ,focus on director and movie theme and main lead characters,
+      if that movie has prequel and sequels then give them and one very closely related movie, give all three as separate json: ${movie} ,using this JSON schema:
+        {json}
+        Return: Array<Movie>`);
+  // console.log(result);
+  return result.response.text();
+}
+
+async function getMovieInfo(name) {
+  try {
+    const page = await wiki.page(name);
+    // console.log(page);
+    //Response of type @Page object
+    const summary = await page.summary();
+    // console.log(summary);
+    return summary;
+    // console.log(response);
+    //Response of type @wikiSummary - contains the intro and the main image
+  } catch (error) {
+    console.log(error);
+    //=> Typeof wikiError
+  }
+}
+
+let movies = moviesData;
+// getMovieInfo('Guntur Kaaram');
+
+app.get("/", (req, res) => {
+  res.send("same-dramas....");
+});
+
+app.get("/home", (req, res) => {
+  let newMovies = movies;
+  res.render("home", { movies });
+});
+
+app.post('/moremovies', async (req, res)=>{
+  let movieName = movies[1].title;
+  movieName = capitalize(movieName);
+  const response = await getMovieInfo(movieName);
+  if (response && response.originalimage && response.title) {
+    let newMovie = {
+      title: response.title,
+      img: response.originalimage.source,
+    };
+    // console.log(newMovie);
+
+    const reccsData = await getSimilarMovies(movieName);
+    if (reccsData) {
+      try {
+        reccsArray = JSON.parse(reccsData);
+
+        if (!Array.isArray(reccsArray)) {
+          throw new Error("Parsed data is not an array");
         }
-        movies.unshift(newMovie);
+
+        reccsArray.forEach( async (recc) => {
+          const reccMoviesRes = await getMovieInfo(recc.title);
+          if (reccMoviesRes && reccMoviesRes.originalimage && reccMoviesRes.title) {
+            let reccMovie = {
+              title: reccMoviesRes.title,
+              img: reccMoviesRes.originalimage.source,
+            };
+          movies.unshift(reccMovie);
+          }
+          console.log(recc);
+        });
+      } catch (error) {
+        console.error("Error parsing reccsData:", error);
+        console.log("Original reccsData string:", reccsData);
+      }
+    } else {
+      console.error("reccsData is undefined. Cannot access movies.");
     }
-    res.redirect('/home');
+  }
+  setTimeout(()=>(res.redirect("/home")), 2000);
 });
 
-app.get('/home', (req,res)=>{
-    let newMovies = movies;
-    res.render('home', {movies});
+app.post("/movie", async (req, res) => {
+  let movieName = req.body.moviename;
+  movieName = capitalize(movieName);
+  const response = await getMovieInfo(movieName);
+  if (response && response.originalimage && response.title) {
+    let newMovie = {
+      title: response.title,
+      img: response.originalimage.source,
+    };
+    // console.log(newMovie);
+
+    const reccsData = await getSimilarMovies(movieName);
+    if (reccsData) {
+      try {
+        reccsArray = JSON.parse(reccsData);
+
+        if (!Array.isArray(reccsArray)) {
+          throw new Error("Parsed data is not an array");
+        }
+
+        reccsArray.forEach( async (recc) => {
+          const reccMoviesRes = await getMovieInfo(recc.title);
+          if (reccMoviesRes && reccMoviesRes.originalimage && reccMoviesRes.title) {
+            let reccMovie = {
+              title: reccMoviesRes.title,
+              img: reccMoviesRes.originalimage.source,
+            };
+          movies.unshift(reccMovie);
+          }
+          console.log(recc);
+        });
+      } catch (error) {
+        console.error("Error parsing reccsData:", error);
+        console.log("Original reccsData string:", reccsData);
+      }
+    } else {
+      console.error("reccsData is undefined. Cannot access movies.");
+    }
+
+    movies.unshift(newMovie);
+    
+  }
+  setTimeout(()=>(res.redirect("/home")), 3000);
 });
 
-app.listen(port, ()=>{
-    console.log(`app listening on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`app listening on http://localhost:${port}`);
 });
---
+++ movies.js
@@ -0,0 +1,22 @@
+module.exports.moviesData = [
+    {
+        title: 'Jersey',
+        img: 'https://tse4.mm.bing.net/th?id=OIP.cQRTJKDBwz_4Avm9WNr2bwHaDt&pid=Api&P=0&h=180',
+    },
+    {
+        title: 'Seetharamam',
+        img: 'https://tse3.mm.bing.net/th?id=OIP.eYISEheQjk7FSFBkjpVZUAHaEK&pid=Api&P=0&h=180',
+    },
+    {
+        title: '12th Fail',
+        img: 'https://tse3.mm.bing.net/th?id=OIP.H6r_tEEEC34rwPUL8l_U_AAAAA&pid=Api&P=0&h=180',
+    },
+    {
+        title: 'Gifted',
+        img: 'https://3.bp.blogspot.com/-sDWFOoAbL54/WU2JT0crVLI/AAAAAAAAEGU/ALJzeckQvEcVrqd9WiS3gTg3ZEKMTm3OwCLcBGAs/s1600/QUAD_UK_Gifted_f.jpg',
+    },
+    {
+        title: 'Dangal',
+        img: 'https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2018/01/featured-dangal-1514956990.jpg',
+    },
+];
